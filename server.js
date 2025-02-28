816589ef0211304ec45b56d798ea087ed2b85b3d
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-02-28 12:15:18 +0100

    signup on mobile

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*"}));
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/mdesigns";
